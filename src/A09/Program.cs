76dbcef6c9bc6a458677c5e3a9fbab9290338f65
Matthew Chudleigh <matthewtoday@gmail.com>
2024-12-09 16:26:42 +1000

    Advent of Code 901

---
+++ src/A09/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+The disk map uses a dense format to represent the layout of files and free space on the disk.
+The digits *alternate* between:
+- length of a file
+- length of free space
+
+- 12345:
+ - one-block file
+ - two blocks of free space
+ - a three-block file
+ - four blocks of free space
+ - five-block file
+ 
+ 
+Each file on disk also has an ID number based on the order of the files as they appear before they are rearranged, starting with ID 0
+- disk map 12345 has three files: 
+  - a one-block file with ID 0, a three-block file with ID 1, and a five-block file with ID 2
+  - Using one character for each block where digits are the file ID and . is free space, the disk map 12345 represents these individual blocks:
+  12345
+  0..111....22222
+  
+  2333133121414131402
+  00...111...2...333.44.5555.6666.777.888899
+  
+- move file blocks *one at a time from the end of the disk to the leftmost free space block* until there are no gaps remaining between file blocks
+- For the disk map 12345, the process looks like this: 
+0..111....22222
+02.111....2222.
+022111....222..
+0221112...22...
+02211122..2....
+022111222......
+
+- calculate the checksum:
+  - add up the result of *multiplying* each of these blocks' position with the file ID number it contains
+  - The leftmost block is in position 0
+  - If a block contains free space, skip it instead
+    00998111
+    ********
+    01234567
+ */
+
+using A09;
+
+//var checksum = Solution.Checksum(@"D:\Source\AdventOfCode2024\data\A09\A09.test-0.txt");
+//Console.WriteLine(checksum);
+var checksum = Solution.Checksum(@"data\A09.data.txt");
+Console.WriteLine(checksum);
---
+++ src/A09/Solution.cs
@@ -0,0 +1,109 @@
+﻿namespace A09;
+
+public static class Solution
+{
+    public static long Checksum(string dataPath)
+    {
+        long checksum = 0;
+        
+        var data = File.ReadAllText(dataPath);
+        var files = new Dictionary<int, (int Id, int Length)>();
+        var freeChunks = new Dictionary<int, int>();
+
+        var id = 0;
+        var index = 0;
+        for (var i = 0; i < data.Length; i++)
+        {
+            var length = data[i] - 48;
+            if (i % 2 == 0)
+            {
+                files[index] = (id, length);
+                id++;
+            }
+            else if (length > 0)
+            {
+                freeChunks[index] = length;
+            }
+
+            index += length;
+        }
+
+        var allFiles = files.OrderByDescending(kv => kv.Value.Id).ToList();
+        foreach (var file in allFiles)
+        {
+            var fileValue = file.Value;
+            while (fileValue.Length > 0)
+            {
+                var free = freeChunks
+                    .Where(kv => kv.Key < file.Key && kv.Value >= 1)
+                    .OrderBy(kv => kv.Key)
+                    .FirstOrDefault();
+
+                if (free.Value > 0)
+                {
+                    freeChunks.Remove(free.Key);
+                    if (free.Value > fileValue.Length)
+                    {
+                        freeChunks[free.Key + fileValue.Length] = free.Value - fileValue.Length;
+                    }
+
+                    if (free.Value >= fileValue.Length)
+                    {
+                        files.Remove(file.Key);
+                        files[free.Key] = fileValue;
+                        fileValue.Length = 0;
+                    }
+                    else
+                    {
+                        fileValue = (fileValue.Id, fileValue.Length - free.Value);
+                        files[file.Key] = fileValue;
+                        files[free.Key] = (fileValue.Id, free.Value);
+                    }
+                }
+                else
+                {
+                    fileValue.Length = 0;
+                }
+            }
+        }
+
+        checksum = CalculateChecksum(files);
+        
+        return checksum;
+    }
+
+    private static long CalculateChecksum(Dictionary<int, (int Id, int Length)> files, bool print = false)
+    {
+        long checksum = 0;
+        var index = 0;
+        foreach (var file in files.OrderBy(kv => kv.Key))
+        {
+            while (index < file.Key)
+            {
+                if (print)
+                {
+                    Console.Write(".");
+                }
+
+                index++;
+            }
+            for (var i = 0; i < file.Value.Length; i++)
+            {
+                if (print)
+                {
+                    Console.Write($"{file.Value.Id}");
+                }
+                
+                checksum += index * file.Value.Id;
+                index++;
+            }
+        }
+
+        if (print)
+        {
+            Console.WriteLine();
+        }
+
+        return checksum;
+    }
+}
