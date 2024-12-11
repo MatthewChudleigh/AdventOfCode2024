a151d87b0f960c7f68b03e3c25a8a5819d1025fa
matthew-chudleigh <matthewtoday@gmail.com>
2024-12-11 16:25:55 +1000

    A11-2 - refactor and support Big Integer for fun

--- src/A11/Solution.cs
+++ src/A11/Solution.cs
@@ -1,66 +1,70 @@
-﻿namespace A11;
+﻿using System.Numerics;
+
+namespace A11;
+
+public static class BigIntExt
+{
+    public static BigInteger Sum(this IEnumerable<BigInteger> bigInts)
+    {
+        BigInteger sum = 0;
+        foreach (var i in bigInts)
+        {
+            sum += i;
+        }
+
+        return sum;
+    }
+}
 
 public static class Solution
 {
-    public class Stone
-    { 
-        public long Value { get; set; }
-        public long Count { get; set; }
-    }
+    public record Stone(BigInteger Value, BigInteger Count);
     
-    //   A        B         2
-    //  A   A   B   B       4
-    // A B A B C C C C      8
-    
-    public static long Solve(string dataPath, int iter)
+    public static BigInteger Solve(string dataPath, int iter)
     {
         var stones = File.ReadAllText(dataPath).Split(" ", StringSplitOptions.RemoveEmptyEntries)
-            .Select(long.Parse)
-            .Select(n => new Stone() { Count = 1, Value = n })
+            .Select(BigInteger.Parse)
+            .Select(n => new Stone(n, 1))
             .ToList();
 
         stones = Generate(stones, iter);
         
-        return stones.Sum(s => s.Count);
+        return stones.Select(s => s.Count).Sum();
     }
 
     public static List<Stone> Generate(List<Stone> stones, int iter)
     {
         while (iter > 0)
         {
-            stones = stones.SelectMany(s =>
-            {
-                if (s.Value == 0)
-                {
-                    return new List<Stone>()
-                    {
-                        new Stone() { Count = s.Count, Value = 1 }
-                    };
-                }
-                
-                var str = $"{s.Value}";
-                if (str.Length % 2 == 0)
-                {
-                    var lhs = str.Substring(0, str.Length / 2);
-                    var rhs = str.Substring(str.Length / 2);
-                    return
-                    [
-                        new Stone() { Count = s.Count, Value = long.Parse(lhs) },
-                        new Stone() { Count = s.Count, Value = long.Parse(rhs) }
-                    ];
-                }
-                else
-                {
-                    return [new Stone() { Count = s.Count, Value = s.Value * 2024 }];
-                }
-            }).GroupBy(s => s.Value).Select(group => new Stone()
-            {
-                Value = group.Key,
-                Count = group.Sum(s => s.Count)
-            }).ToList();
+            stones = stones.SelectMany(SplitStone)
+                .GroupBy(s => s.Value)
+                .Select(group => new Stone(group.Key, group.Select(s => s.Count).Sum()))
+                .ToList();
             iter--;
         }
 
         return stones;
+    }
+
+    private static IEnumerable<Stone> SplitStone(Stone s)
+    {
+        if (s.Value == 0)
+        {
+            return new List<Stone>()
+            {
+                s with { Value = 1 }
+            };
+        }
+            
+        var str = $"{s.Value}";
+        if (str.Length % 2 != 0) return [s with { Value = s.Value * 2024 }];
+        
+        var lhs = str[..(str.Length / 2)];
+        var rhs = str[(str.Length / 2)..];
+        return
+        [
+            s with { Value = BigInteger.Parse(lhs) },
+            s with { Value = BigInteger.Parse(rhs) }
+        ];
     }
 }
--- test/A11.Test/Test.cs
+++ test/A11.Test/Test.cs
@@ -1,11 +1,13 @@
-﻿namespace A11.Test;
+﻿using System.Numerics;
+
+namespace A11.Test;
 
 public class Test
 {
     [Fact]
     public void TestZero()
     {
-        var stones = Solution.Generate([new Solution.Stone(){ Count = 1, Value = 0 }], 1);
+        var stones = Solution.Generate([new (0, 1)], 1);
         Assert.Single(stones);
         Assert.Equal(1, stones[0].Count);
         Assert.Equal(1, stones[0].Value);
@@ -14,7 +16,7 @@ public class Test
     [Fact]
     public void TestOne()
     {
-        var stones = Solution.Generate([new Solution.Stone(){ Count = 1, Value = 1 }], 1);
+        var stones = Solution.Generate([new (1, 1)], 1);
         Assert.Single(stones);
         Assert.Equal(1, stones[0].Count);
         Assert.Equal(2024, stones[0].Value);
@@ -23,7 +25,7 @@ public class Test
     [Fact]
     public void TestEvenSize()
     {
-        var stones = Solution.Generate([new Solution.Stone(){ Count = 1, Value = 11 }], 1);
+        var stones = Solution.Generate([new (11, 1)], 1);
         Assert.Single(stones);
         Assert.Equal(2, stones[0].Count);
         Assert.Equal(1, stones[0].Value);
@@ -32,7 +34,7 @@ public class Test
     [Fact]
     public void TestEvenSizeIter2()
     {
-        var stones = Solution.Generate([new Solution.Stone(){ Count = 1, Value = 11 }], 2);
+        var stones = Solution.Generate([new(11, 1)], 2);
         Assert.Single(stones);
         Assert.Equal(2, stones[0].Count);
         Assert.Equal(2024, stones[0].Value);
@@ -41,8 +43,8 @@ public class Test
     [Fact]
     public void TestIters()
     {
-        var initialSet = new List<Solution.Stone>
-            { new Solution.Stone() { Count = 1, Value = 125 }, new Solution.Stone() { Count = 1, Value = 17 } };
+        var initialSet = new List<Solution.Stone> { new(125, 1), new(17, 1) };
+        
         var stones = Solution.Generate(initialSet, 1);
         Assert.Equal(3, stones.Count);
         Assert.Equal(253000, stones[0].Value);
@@ -64,10 +66,14 @@ public class Test
         Assert.Equal(24, stones[3].Value);
         Assert.Equal(28676032, stones[4].Value);
 
-        var stoneCount = Solution.Generate(initialSet, 6).Sum(x => x.Count);
+        var stoneCount = Solution.Generate(initialSet, 6).Select(x => x.Count).Sum();
         Assert.Equal(22, stoneCount);
 
-        stoneCount = Solution.Generate(initialSet, 25).Sum(x => x.Count);
+        stoneCount = Solution.Generate(initialSet, 25).Select(x => x.Count).Sum();
         Assert.Equal(55312, stoneCount);
+
+        stoneCount = Solution.Generate(initialSet, 300).Select(x => x.Count).Sum();
+        var bigint = BigInteger.Parse("4599266493873511347027613123865463965626814547987706378");
+        Assert.Equal(bigint, stoneCount);
     }
 }
