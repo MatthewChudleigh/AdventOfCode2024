1ea7205d660b087bc465c3b50aafc538df6f02aa
Matthew Chudleigh <matthewtoday@gmail.com>
2024-12-21 23:57:13 +1000

    wip

--- src/A21/Solution.cs
+++ src/A21/Solution.cs
@@ -4,9 +4,156 @@ namespace A21;
 
 public static class Solution
 {
-    public class Keypad
+    public static Keypad Keypad = new Keypad();
+    public static Arrows Arrows = new Arrows();
+    
+    public static (string moves, long total) Calculate(char start, string code, int robots)
     {
-        public Dictionary<char, (int X, int Y)> Keys = new()
+        var moves = GetAllMoves(Keypad, start, code, robots);
+        long.TryParse(code[..^1], out var val);
+        return (moves.moves, total: moves.total * val);
+    }
+    
+    public static (string moves, long total) GetAllMoves(InputDevice input, char start, string code, int robots)
+    {
+        var total = 0L;
+
+        var pos = input.Keys[start];
+        var allMoves = new StringBuilder();
+        foreach (var c in code)
+        {
+            var end = input.Keys[c];
+            
+            var dx = pos.X < end.X ? 1 : pos.X > end.X ? -1 : 0;
+            var dy = pos.Y < end.Y ? 1 : pos.Y > end.Y ? -1 : 0;
+
+            if ((dx != 0 && Costs.TryGetValue((pos, (X: dx, Y: 0), end, robots), out var cost))
+                || (dy != 0 && Costs.TryGetValue((pos, (X: 0, Y: dy), end, robots), out cost)))
+            {
+                total += cost;
+                pos = end;
+                continue;
+            }
+            
+            var toVisit = new PriorityQueue<((int X, int Y) Pos, (int X, int Y) Dir, StringBuilder Moves), int>();
+
+            if (dx != 0)
+            {
+                toVisit.Enqueue(((X: pos.X+dx, pos.Y), (dx, 0), new StringBuilder($"{start}")), 0);
+            }
+            if (dy != 0)
+            {
+                toVisit.Enqueue(((X: pos.X, Y: pos.Y + dy), (0, dy), new StringBuilder($"{start}")), 0);
+            }
+
+            var current = 0L;
+            var visited = new HashSet<(int X, int Y)>();
+            
+            while (toVisit.TryDequeue(out var cur, out var dist))
+            {
+                if (!input.Map.TryGetValue(cur.Pos, out var n)) continue;
+                if (cur.Pos == end)
+                {
+                    var moves = cur.Moves.ToString();
+                    if (robots == 0)
+                    {
+                        current += moves.Length;
+                        allMoves.Append(moves);
+                    }
+                    else
+                    {
+                        var x = new StringBuilder(moves);
+                        x.Append(c);
+                        var (m, cost1) = GetAllMoves(Arrows, c, moves, robots - 1);
+                        current += cost1;
+                    }
+                    break;
+                }
+
+                if (!visited.Add(cur.Pos)) continue;
+                
+                dx = cur.Pos.X < end.X ? 1 : cur.Pos.X > end.X ? -1 : 0;
+                dy = cur.Pos.Y < end.Y ? 1 : cur.Pos.Y > end.Y ? -1 : 0;
+
+                var next = new StringBuilder(cur.Moves.ToString());
+                next.Append(c);
+                
+                if (dx != 0)
+                {
+                    toVisit.Enqueue(((X: cur.Pos.X + dx, Y: cur.Pos.Y), (dx, 0), next), dist + 1);
+                }
+                if (dy != 0)
+                {
+                    toVisit.Enqueue(((X: cur.Pos.X, Y: cur.Pos.Y + dy), (0, dy), next), dist + 1);
+                }
+            }
+
+            total += current;
+            pos = end;
+        }
+        
+        return (allMoves.ToString(), total);
+    }
+
+    public static Dictionary<((int X, int Y) Pos, (int X, int Y) Dir, (int X, int Y) Tgt, int Robot), int> Costs = new();
+}
+
+public class InputDevice
+{
+    public char Pos { get; set; } = 'A';
+    public Dictionary<char, (int X, int Y)> Keys = new();
+    public Dictionary<(int X, int Y), char> Map = new();
+    public Dictionary<(char From, char To), string> Moves = new();
+
+    public void Init()
+    {
+        foreach (var move in Moves.ToList())
+        {
+            Moves[(move.Key.To, move.Key.From)] = move.Value switch
+            {
+                "<" => ">",
+                "v" => "^",
+                "^" => "v",
+                _ => move.Value
+            };
+        }
+        
+        foreach (var key in Keys)
+        {
+            Map[key.Value] = key.Key;
+        }
+    }
+}
+
+public class Arrows : InputDevice
+{
+    public Arrows()
+    {
+        Keys = new Dictionary<char, (int X, int Y)>
+        {
+            { '^', (-1, 0) },
+            { 'A', (0, 0) },
+            { '<', (-2, -1) },
+            { 'v', (-1, -1) },
+            { '>', (0, -1) },
+        };
+        Moves = new Dictionary<(char From, char To), string>
+        {
+            {('A', '^'), "<"},
+            {('A', '>'), "v"},
+            {('^', 'v'), "v"},
+            {('v', '<'), "<"},
+            {('>', 'v'), "<"},
+        };
+        Init();
+    }
+}
+
+public class Keypad : InputDevice
+{
+    public Keypad()
+    {
+        Keys = new Dictionary<char, (int X, int Y)>
         {
             { 'A', (0, 0) },
             { '0', (-1, 0) },
@@ -20,10 +167,8 @@ public static class Solution
             { '8', (-1, 3) },
             { '7', (-2, 3) }
         };
-
-        public Dictionary<(int X, int Y), char> Map { get; } = new();
         
-        public Dictionary<(char From, char To), string> Moves { get; } = new() 
+        Moves = new Dictionary<(char From, char To), string>
         {
             {('A', '0'), "<"},
             {('A', '3'), "^"},
@@ -37,65 +182,10 @@ public static class Solution
             {('5', '4'), "<"},
             {('6', '9'), "^"},
             {('5', '8'), "^"},
-            {('4', '7'), "<"},
+            {('4', '7'), "^"},
             {('9', '8'), "<"},
             {('8', '7'), "<"},
         };
-
-        public void Init()
-        {
-            foreach (var move in Moves.ToList())
-            {
-                Moves[(move.Key.To, move.Key.From)] = move.Value switch
-                {
-                    "<" => ">",
-                    "^" => "v",
-                    _ => move.Value
-                };
-            }
-            
-            foreach (var key in Keys)
-            {
-                Map[key.Value] = key.Key;
-            }
-        }
-
-        public void GetMoves(StringBuilder sb, char from, char to)
-        {
-            var c1 = from;
-            var pos = Keys[from];
-            var end = Keys[to];
-            
-            while (pos != end)
-            {
-                var dx = pos.X < end.X ? 1 : pos.X > end.X ? -1 : 0;
-                var dy = pos.Y < end.Y ? 1 : pos.Y > end.Y ? -1 : 0;
-
-                var next = (X: pos.X + dx, Y: pos.Y);
-                if (dx == 0 || !Map.ContainsKey(next))
-                {
-                    next = (X: pos.X, Y: pos.Y + dy);
-                }
-                var c2 = Map[next];
-                var move = Moves[(c1, c2)];
-                c1 = c2;
-                sb.Append(move);
-                pos = next;
-            }
-        }
-        
-
-        public string GetMoves(string code)
-        {
-            var moves = new StringBuilder();
-            var pos = code[0];
-            foreach (var c in code.Skip(1))
-            {
-                GetMoves(moves, pos, c);
-                pos = c;
-            }
-
-            return moves.ToString();
-        }
+        Init();
     }
 }
--- test/A21.Test/Test.cs
+++ test/A21.Test/Test.cs
@@ -1,19 +1,42 @@
-﻿namespace A21.Test;
+﻿using Xunit.Abstractions;
+
+namespace A21.Test;
 
 public class Test
 {
-    [Theory]
-    [InlineData("A0", "<")]
-    [InlineData("A3", "^")]
-    [InlineData("A2", "<^")]
-    [InlineData("A8", "<^^^")]
-    [InlineData("10", ">v")]
-    [InlineData("4A", ">>vv")]
-    public void TestKeypad(string code, string expectedMoves)
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public Test(ITestOutputHelper testOutputHelper)
     {
-        var keypad = new Solution.Keypad();
-        keypad.Init();
-        var moves = keypad.GetMoves(code);
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Theory]
+    [InlineData(1, 'A', "0", "<A", 0)]
+    [InlineData(1, 'A', "1", "^<<A", 4)]
+    [InlineData(1, '1', "7", "^^A", 21)]
+    [InlineData(1, 'A', "3", "^A", 6)]
+    [InlineData(1, 'A', "2", "^<A", 6)]
+    [InlineData(1, 'A', "8", "^^^<A", 40)]
+    [InlineData(1, '1', "0", ">vA", 0)]
+    [InlineData(1, '4', "A", ">>vvA", 0)]
+    [InlineData(1, 'A', "029A", "<A^A>^^AvvvA", 348)]
+    [InlineData(1, 'A', "179A", "^<<A^^A>>AvvvA", 2506)]
+    [InlineData(2, 'A', "0", "v<<A>>^A", 0)]
+    [InlineData(2, 'A', "029A", "v<<A>>^A<A>AvA^<AA>Av<AAA>^A", 812)]
+    [InlineData(3, 'A', "029A", "v<A<AA>>^AvAA^<A>Av<<A>>^AvA^Av<A>^A<Av<A>>^AAvA^Av<A<A>>^AAAvA^<A>A", 68 * 29)]
+    [InlineData(3, 'A', "980A", "v<<A>>^AAAvA^Av<A<AA>>^AvAA^<A>Av<A<A>>^AAAvA^<A>Av<A>^A<A>A", 60 * 980)]
+    [InlineData(3, 'A', "179A", "v<<A>>^Av<A<A>>^AAvAA^<A>Av<<A>>^AAvA^Av<A>^AA<A>Av<A<A>>^AAAvA^<A>A", 68 * 179)]
+    [InlineData(3, 'A', "456A", "v<<A>>^AAv<A<A>>^AAvAA^<A>Av<A>^A<A>Av<A>^A<A>Av<A<A>>^AAvA^<A>A", 64 * 456)]
+    
+    [InlineData(1, 'A', "379A", "^A^^<<A>>AvvvA", 5306)]
+    [InlineData(2, 'A', "379A", "<A>A<AAv<AA>>^AvAA^Av<AAA>^A", 10612)]
+    [InlineData(3, 'A', "379A", "<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A", 64 * 379)]
+    public void TestSolution(int robots, char start, string code, string expectedMoves, int expectedTotal)
+    {
+        var (moves, total) = Solution.Calculate(start, code, robots);
+        _testOutputHelper.WriteLine(moves);
+        Assert.Equal(expectedTotal, total);
         Assert.Equal(expectedMoves, moves);
     }
 }
