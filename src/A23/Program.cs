3e3670a4e59b762cc6ff8a5089cb8107ee386070
matthew <matthewtoday@gmail.com>
2024-12-24 06:13:21 +1000

    wip

--- src/A23/Program.cs
+++ src/A23/Program.cs
@@ -8,3 +8,6 @@ var data = File.ReadAllLines(Path.Combine(baseDir!, "A23.data.txt"));
 
 var count = Solution.Solve('t', data);
 Console.WriteLine(count);
+var dict = Solution.ToDict(data);
+var password = Solution.SolvePart2(dict);
+Console.WriteLine(password);
--- src/A23/Solution.cs
+++ src/A23/Solution.cs
@@ -4,9 +4,15 @@ public static class Solution
 {
     public static int Solve(char t, string[] data)
     {
+        var dict = ToDict(data);
+        return Solve(t, dict);
+    }
+
+    public static Dictionary<string, HashSet<string>> ToDict(string[] data)
+    {
         var dict = new Dictionary<string, HashSet<string>>();
         foreach (var kv in data
-                 .Select(l => l.Split('-')))
+                     .Select(l => l.Split('-')))
         {
             if(!dict.TryGetValue(kv[0], out var p0))
             {
@@ -23,7 +29,44 @@ public static class Solution
             p1.Add(kv[0]);
         }
 
-        return Solve(t, dict);
+        return dict;
+    }
+
+    public static string SolvePart2(Dictionary<string, HashSet<string>> dict)
+    {
+        List<string> largest = [];
+        var check = new HashSet<string>();
+        foreach (var kv in dict)
+        {
+            check.Add(kv.Key);
+            foreach (var v in kv.Value.Where(v => !check.Contains(v)))
+            {
+                var code = Calculate(dict, [v, kv.Key]);
+                if (code.Count > largest.Count)
+                {
+                    largest = code;
+                }
+            }
+        }
+
+        return string.Join(",", largest.Order());
+    }
+
+    static List<string> Calculate(Dictionary<string, HashSet<string>> dict, List<string> set)
+    {
+        var largest = set;
+        foreach (var v in dict[set[0]])
+        {
+            if(set.Contains(v)) continue;
+            if(set.Any(s => !dict[v].Contains(s))) continue;
+            var l = Calculate(dict, [v, ..set]);
+            if (l.Count > largest.Count)
+            {
+                largest = l;
+            }
+        }
+        
+        return largest;
     }
     
     public static int Solve(char t, Dictionary<string, HashSet<string>> pairs)
